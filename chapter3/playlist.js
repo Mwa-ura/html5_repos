c4bcd9015fe768ce702632fe6abaf2d99315e2f1
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2023-01-16 03:30:28 +0400

    Add chapter3 folder

---
+++ chapter3/playlist.js
@@ -0,0 +1,11 @@
+// Root javascript
+window.onload = init;
+
+function init() {
+    var button = document.getElementById("addButton");
+    button.onclick = handleButtonClick;
+}
+
+function handleButtonClick() {
+    alert("Button was clicked!");
+}
