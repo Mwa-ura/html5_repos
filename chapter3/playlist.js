336bbe96e8cd0878b751d351df9e09b712b459e6
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2023-01-17 03:10:13 +0400

    Add new <li> to the DOM

--- chapter3/playlist.js
+++ chapter3/playlist.js
@@ -10,10 +10,16 @@ function handleButtonClick() {
     var textInput = document.getElementById("songTextInput")
     var songName = textInput.value;
     // Test for null value
-    if (songName == "") {
-        alert("Please add a song");
+    if (songName == "" ) {
+      alert("You have not entered a song");
     } else {
-        alert("Adding " + songName);
+   
+        // Create a new element <li> to hold a new song
+        var li = document.createElement("li");
+        li.innerHTML = songName;
+
+        // Add the element to the DOM
+        var ul = document.getElementById("playlist")
+        ul.appendChild(li);
     }
-    
 }
