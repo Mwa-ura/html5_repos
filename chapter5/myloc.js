f1aa06611c403299815801f7e2033dfcafb2637e
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2023-01-25 03:06:15 +0400

    Create error handler function

--- chapter5/myloc.js
+++ chapter5/myloc.js
@@ -2,12 +2,12 @@ window.onload = getMyLocation;
 
 function getMyLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(displayLocation);
+        navigator.geolocation.getCurrentPosition(displayLocation, displayError);
     }
     else {
         alert("Your browser doesn't support Geolocation API!");
     }
-};
+}
 // Handler to display location
 function displayLocation(position) {
     var latitude = position.coords.latitude;
@@ -16,4 +16,19 @@ function displayLocation(position) {
     var div = document.getElementById("location");
     div.innerHTML = "You are at Latitude " +latitude+ " and Longitude " +longitude+"."
 
+}
+// Error handler
+function displayError(error) {
+    var errorTypes = {
+        0: "Unknown error",
+        1: "Permission denied by user",
+        2: "Position is not available",
+        3: "Request timeout"
+    };
+    var errorMessage = errorTypes[error.code];
+    if (error.code == 0 || error.code == 2) {
+        errorMessage = errorMessage +" "+ error.message;
+    }
+    var div = document.getElementById("location");
+    div.innerHTML = errorMessage;
 }
