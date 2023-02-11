63964e56e68e366aecef4bd3f4fffa0045b2bdc4
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2023-02-12 03:47:50 +0400

    Detect whether browser supports csnvas

--- chapter7/canvas.js
+++ chapter7/canvas.js
@@ -1,5 +1,11 @@
 window.onload = function() {
     var canvas = document.getElementById("tweetShirt");
     var context = canvas.getContext("2d");
-    context.fillRect(10, 10, 100, 100);
+    // Check whether the browser supports canvas
+    if (context) {
+        context.fillRect(10, 10, 100, 100);
+    }
+    else {
+        alert("Your browser don't support canvas");
+    }
 }
