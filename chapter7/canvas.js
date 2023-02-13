e0104e8c4b4298ea41e88fd350aeec746b8dee2a
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2023-02-14 03:26:36 +0400

    Fix bug

--- chapter7/canvas.js
+++ chapter7/canvas.js
@@ -1,20 +1,31 @@
 window.onload = function() {
     var button = document.getElementById("previewButton");
-    canvas.onclick = previewHandler;
-}
+    button.onclick = previewHandler;
+};
 
 function previewHandler() {
-    var selectOj = document.getElementById("canvas");
+    var canvas = document.getElementById("tweetShirt");
     var context = canvas.getContext("2d");
     // Check which shape have been selected
     // Get its index and assign it to variable shape
     var selectOj = document.getElementById("shape");
-    var index = selectOj.selectedIdex;
+    var index = selectOj.selectedIndex;
     var shape = selectOj[index].value;
     // If shape selected is square render the 20 squares
-    if (shape == "square") {
+    if (shape == "squares") {
         for (var squares = 0; squares < 20; squares++ ) {
-            drawSquare(canvas, context)
+            drawSquare(canvas, context);
         }
-    };
+    }
+    else {
+        alert("Are you sure you don't want any shape!");
+    }
+};
+// Function to calculate w, x and y axis
+function drawSquare(canvas, context) {
+    var w = Math.floor(Math.random() * 40);
+    var x = Math.floor(Math.random() * canvas.width);
+    var y = Math.floor(Math.random() * canvas.height);
+    context.fillStyle = "lightblue"; // Shape color
+    context.fillRect(x, y, w, w);
 }
