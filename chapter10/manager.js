e267e1d3691d23262af10d9b44b2aa19a4d986b9
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2023-04-09 03:48:16 +0400

    Update to create an array of webWorkers

--- chapter10/manager.js
+++ chapter10/manager.js
@@ -1,10 +1,15 @@
 window.onload = function () {
-    var worker = new Worker("worker.js");
-    worker.postMessage("ping");
-    worker.postMessage([1,2,3,4,5]);
-    worker.postMessage({"name":'Mwa ura', "blog": "Mwaura"});
-    worker.onmessage = function(event) {
-        var message = "Worker say " +event.data;
-        document.getElementById('output').innerHTML = message;
+    var numWorkers = 3;
+    var workers = [];
+    // Iterate the workers
+    for (var i = 0; i < numWorkers; i++) {
+        var worker = new Worker("worker.js");
+        worker.onmessage = function(event) {
+            alert(event.target +" Says "+ event.data);
+        };
+        workers.push(worker);
+    }
+    for (var i = 0; i < workers.length; i++) {
+        workers[i].postMessage("ping");
     }
 }
